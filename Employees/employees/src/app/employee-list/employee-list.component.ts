cbdba286fa8a6a02085b6aebcec9f68693dbd689
EzeJuarez <ezequieledjuarez@gmail.com>
2021-10-12 21:29:37 -0300

    Add functionality services, route and styles to getEmployees

--- Employees/employees/src/app/employee-list/employee-list.component.ts
+++ Employees/employees/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Employee} from "../employee"
+import {EmployeeService} from "../employee.service";
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -9,9 +10,16 @@ export class EmployeeListComponent implements OnInit {
 
   employees : Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    })
   }
 
 }
