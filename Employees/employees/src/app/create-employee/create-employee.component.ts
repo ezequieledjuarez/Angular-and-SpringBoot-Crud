5224b35ab58115b47e48eaf375744efce30f28e8
EzeJuarez <ezequieledjuarez@gmail.com>
2021-10-17 08:02:46 -0300

    Add update component and search by id

--- Employees/employees/src/app/create-employee/create-employee.component.ts
+++ Employees/employees/src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Employee} from "../employee";
+import {EmployeeService} from "../employee.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-employee',
@@ -10,12 +12,25 @@ export class CreateEmployeeComponent implements OnInit {
 
   employee: Employee = new Employee();
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){
+  saveEmployee(){
+    this.employeeService.createEmployee(this.employee).subscribe(data =>{
+      console.log(data)
+      this.goToEmployeeList();
+    },
+      error => console.log(error));
+  }
 
+  goToEmployeeList(){
+    this.router.navigate(['/employees']);
+  }
+  onSubmit() {
+    console.log(this.employee)
+    this.saveEmployee();
   }
 }
--- Employees/employees/src/app/employee.service.ts
+++ Employees/employees/src/app/employee.service.ts
@@ -15,4 +15,16 @@ export class EmployeeService {
   getEmployeesList(): Observable<Employee[]>{
   return this.httpClient.get<Employee[]>(`${this.baseURL}`);
   }
+
+  createEmployee(employee: Employee): Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`, employee);
+  }
+
+  getEmployeeById(id: number): Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+  }
+
+  updateEmployee(id:  number, employee: Employee): Observable<Object>{
+    return this.httpClient.put(`${this.baseURL}/${id}`, employee);
+  }
 }
