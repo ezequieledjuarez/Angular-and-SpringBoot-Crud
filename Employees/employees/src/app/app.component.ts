ae72c529707063d534e7b2a6d45f644e5e19bb5e
EzeJuarez <ezequieledjuarez@gmail.com>
2021-10-17 08:45:34 -0300

    Add delete employee

--- Employees/employees/src/app/app.component.ts
+++ Employees/employees/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Employees List';
+  title = 'Employees Manager';
   year: number = new Date().getFullYear()
 }
